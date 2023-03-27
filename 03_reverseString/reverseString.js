2695d5bc644d012ef1adb0fd3ef6cdcf8f478b82
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-03-27 00:35:30 -0500

    solved reverseString using string.split(), array.pop(), and array.join()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    if(typeof string !== 'string') {
+        return "ERROR";
+    }
 
+    let stringArray = string.split('')
+    let reversedArray = [];
+    
+    for(let i = 0; i < string.length; i++) {
+        reversedArray.push(stringArray.pop())
+    }
+    
+    return reversedArray.join('');
 };
-
 // Do not edit below this line
 module.exports = reverseString;
