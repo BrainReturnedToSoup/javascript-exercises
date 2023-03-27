f7a75d91e19835ed6c706f5a3e649d356918851e
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-03-27 00:48:41 -0500

    solved sumAll using a for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    
+    let smallerInt;
+    let largerInt;
+    let sum = 0;
+    
+    if(int1 > int2) {
+        smallerInt = int2;
+        largerInt = int1;
+    } else {
+        smallerInt = int1;
+        largerInt = int2;
+    }
+
+    for(let i = smallerInt; i < largerInt+1; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
 // Do not edit below this line
+sumAll(123, 1);
 module.exports = sumAll;
