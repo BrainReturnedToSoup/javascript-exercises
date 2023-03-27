a12216a07bfc325764bc6ed3774ef90a58db1ceb
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-03-27 01:48:03 -0500

    solved tempConversion by implementing temp formulas with math.round() and control flow

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+    const output = Math.round(((inputTemp-32)*5)/9).toFixed(1);
+    const roundedOutput = Math.round(output);
+    if(output - roundedOutput === 0) {
+      return roundedOutput;
+    } else {
+      return output;
+    }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+    const output = Math.round((inputTemp*1.8)+32).toFixed(1);
+    const roundedOutput = Math.round(output);
+    if(output - roundedOutput === 0) {
+      return roundedOutput;
+    } else {
+      return output;
+    }
 };
 
 // Do not edit below this line
