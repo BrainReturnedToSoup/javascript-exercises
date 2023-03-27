dc39a0a67cf325de2dbff31113c8865296ffe25e
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-03-27 00:37:36 -0500

    changed to optimal solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,15 +3,8 @@ const reverseString = function(string) {
     if(typeof string !== 'string') {
         return "ERROR";
     }
-
-    let stringArray = string.split('')
-    let reversedArray = [];
     
-    for(let i = 0; i < string.length; i++) {
-        reversedArray.push(stringArray.pop())
-    }
-    
-    return reversedArray.join('');
-};
+    return string.split('').reverse().join('');
+}
 // Do not edit below this line
 module.exports = reverseString;
