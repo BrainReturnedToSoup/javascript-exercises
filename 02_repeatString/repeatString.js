5bbbd8d5be224db2671d5deb7f3fb2b8074b4432
BrainReturnedToSoup <minton.nikkolas@gmail.com>
2023-03-26 22:16:01 -0500

    completed and tested repeatString.js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
-
+const repeatString = (string, timesToRepeat) => {
+    if(string < 0 || timesToRepeat < 0) return "ERROR";
+    let output = ''
+    for(let i = 0; i < timesToRepeat; i++) {
+      output += string;
+    }
+    return output;
+}
 // Do not edit below this line
 module.exports = repeatString;
